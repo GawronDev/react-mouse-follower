05232a9c95da3c80c3acc0c8f2efbe79a40eb82e
GawronDev <kubusgawron@gmail.com>
2023-04-11 15:09:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className='header-container'>
+      <h3>Bring your</h3>
+      <h1>Dream web vision</h1>
+      <h3>To life</h3>
+    </div>
+  )
+}
+
+export default App
