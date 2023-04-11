ab4192793c4a2062cfe560753c3eb7a151a49475
GawronDev <kubusgawron@gmail.com>
2023-04-11 16:24:20 +0200

    Created the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import Cursor from './components/Cursor'
 
 function App() {
   return (
-    <div className='header-container'>
-      <h3>Bring your</h3>
-      <h1>Dream web vision</h1>
-      <h3>To life</h3>
+    <div className='main-wrapper'>
+      <Cursor />
+      <div className='header-container'>
+        <h3>Bring your</h3>
+        <h1>Dream web vision</h1>
+        <h3>To life</h3>
+      </div>
     </div>
   )
 }
---
+++ src/components/Cursor.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef } from "react";
+
+export default function Cursor() {
+    const cursorRef = useRef(null);
+
+    useEffect(()=>{
+
+    }, [])
+    return(
+        <div className="cursor"></div>
+    )
+}
